57010dd94c529c4320aa90ef077a81b886b81396
Rodrigodx <rodrigodx52@gmail.com>
2023-03-08 15:49:50 -0300

    service layer, component registration

---
+++ src/main/java/com/rodrigodx/aprendendo/services/UserService.java
@@ -0,0 +1,29 @@
+package com.rodrigodx.aprendendo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rodrigodx.aprendendo.entities.User;
+import com.rodrigodx.aprendendo.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
+
+	
