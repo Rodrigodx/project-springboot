c3ef490595354be8bf28d3d277bf49f5dff3f735
Rodrigodx <rodrigodx52@gmail.com>
2023-03-21 14:56:49 -0300

    Exception handling - update

--- src/main/java/com/rodrigodx/aprendendo/services/UserService.java
+++ src/main/java/com/rodrigodx/aprendendo/services/UserService.java
@@ -13,6 +13,8 @@ import com.rodrigodx.aprendendo.repositories.UserRepository;
 import com.rodrigodx.aprendendo.services.exceptions.DatabaseException;
 import com.rodrigodx.aprendendo.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -44,9 +46,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		}catch(EntityNotFoundException  e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
